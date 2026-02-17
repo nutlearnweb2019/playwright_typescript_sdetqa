43eaae9fc80deb9a47cfbb6ef2627207efaa130a
Thanajsil Janthachot <51187331+nutlearnweb2019@users.noreply.github.com>
2026-02-17 18:33:32 +0700

    Setup ENV and Writing Tests (C1)

---
+++ tests/1.my-test.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test.only('Verify page title', async ({page}) => {
+    await page.goto('https://unsplash.com/');
+
+    const title: string = await page.title();
+    console.log('Title: ', title);
+
+    await expect(page).toHaveTitle('Beautiful Free Images & Pictures | Unsplash');
+});
+
